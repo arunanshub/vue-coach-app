d0f7a3cba7778740f2616beb035320fe15aee761
Arunanshu Biswas <mydellpc07@gmail.com>
2023-04-13 20:36:14 +0530

    feat(app): slots and cards

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const goals = reactive(['finish course', 'learn vue'])
+</script>
+
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal"></slot>
+      <slot name="f" :items="'e'"></slot>
+    </li>
+  </ul>
+</template>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import BaseBadge from './BaseBadge.vue'
+import BaseCard from './BaseCard.vue'
+
+defineProps<{
+  fullName: string
+  infoText: string
+  role: string
+}>()
+</script>
+
+<template>
+  <section>
+    <BaseCard>
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <BaseBadge :type="role" :caption="role.toUpperCase()" />
+      </template>
+      <p>{{ infoText }}</p>
+    </BaseCard>
+  </section>
+</template>
