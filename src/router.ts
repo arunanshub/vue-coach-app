3f0d51d0809309bd82c4a64a2b062249ca032060
Arunanshu Biswas <mydellpc07@gmail.com>
2023-04-30 16:37:05 +0530

    refactor(router): split code into smaller chunks

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import NotFound from '@/pages/NotFound.vue'
-import CoachDetail from '@/pages/coaches/CoachDetail.vue'
-import CoachRegistration from '@/pages/coaches/CoachRegistration.vue'
-import CoachesList from '@/pages/coaches/CoachesList.vue'
-import ContactCoach from '@/pages/requests/ContactCoach.vue'
-import RequestsReceived from '@/pages/requests/RequestsReceived.vue'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { name: 'coaches', path: '/coaches', alias: '/', component: CoachesList },
+    {
+      name: 'coaches',
+      path: '/coaches',
+      alias: '/',
+      component: () => import('@/pages/coaches/CoachesList.vue'),
+    },
     {
       name: 'coach',
       path: '/coaches/:id',
-      component: CoachDetail,
+      component: () => import('@/pages/coaches/CoachDetail.vue'),
       props: true,
-      children: [{ name: 'contact', path: 'contact', component: ContactCoach }],
+      children: [
+        {
+          name: 'contact',
+          path: 'contact',
+          component: () => import('@/pages/requests/ContactCoach.vue'),
+        },
+      ],
     },
-    { name: 'register', path: '/register', component: CoachRegistration },
-    { name: 'requests', path: '/requests', component: RequestsReceived },
-    { name: 'not-found', path: '/:notFound(.*)', component: NotFound },
+    {
+      name: 'register',
+      path: '/register',
+      component: () => import('@/pages/coaches/CoachRegistration.vue'),
+    },
+    {
+      name: 'requests',
+      path: '/requests',
+      component: () => import('@/pages/requests/RequestsReceived.vue'),
+    },
+    {
+      name: 'not-found',
+      path: '/:notFound(.*)',
+      component: () => import('@/pages/NotFound.vue'),
+    },
   ],
 })
