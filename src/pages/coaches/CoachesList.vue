5eee0680ae0037604147e4138dd6a9e668bfac69
Arunanshu Biswas <mydellpc07@gmail.com>
2023-05-02 13:09:34 +0530

    feat(CoachesList): show spinner or error message based on condition

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,14 +7,31 @@ import CoachFilter, {
 import CoachItem from '@/components/coaches/CoachItem.vue'
 import BaseButton from '@/components/ui/BaseButton.vue'
 import BaseCard from '@/components/ui/BaseCard.vue'
+import BaseDialog from '@/components/ui/BaseDialog.vue'
+import BaseSpinner from '@/components/ui/BaseSpinner.vue'
+import { useAuthStore } from '@/stores/auth'
 import { useCoachStore } from '@/stores/coaches'
 
-// get the coach data
+const authStore = useAuthStore()
+
+// get the coach store
 const coachStore = useCoachStore()
+
+// load the coaches here or set error message if error occurs
+const errorMessage = ref<string>()
+function loadCoaches(forceRefresh = false) {
+  errorMessage.value = undefined
+  coachStore.loadCoaches(forceRefresh).catch((error: Error) => {
+    errorMessage.value = error.message
+  })
+}
+loadCoaches()
+
 const { coaches, hasCoaches } = storeToRefs(coachStore)
 
 // the `areas` filters that are currently applied to the Coaches list.
 const activeFilters = ref<AreaFilter>({})
+
 function setFilters(updatedFilters: AreaFilter) {
   activeFilters.value = updatedFilters
 }
@@ -27,28 +44,45 @@ const filteredCoaches = computed(() => {
     })
   })
 })
+
+const canRegisterAsCoach = computed(() => {
+  return !coachStore.isLoading &&
+    authStore.isLoggedIn &&
+    !coachStore.isUserCoach(authStore.userId!)
+})
 </script>
 
 <template>
-  <CoachFilter @change-filter="setFilters" />
-  <BaseCard>
-    <section>
-      <div class="controls">
-        <BaseButton mode="outline">Refresh</BaseButton>
-        <BaseButton to-name="register" v-if="!coachStore.isUserCoach">
-          Register as Coach
-        </BaseButton>
-      </div>
-      <ul v-if="hasCoaches">
-        <CoachItem
-          v-for="coach in filteredCoaches"
-          :coach="coach"
-          :key="coach.id"
-        />
-      </ul>
-      <h3 v-else>No coaches found.</h3>
-    </section>
-  </BaseCard>
+  <div>
+    <CoachFilter @change-filter="setFilters" />
+    <BaseDialog
+      :show="!!errorMessage"
+      title="An Error Occurred!"
+      @close="loadCoaches"
+    >
+      Failed to load coaches data: {{ errorMessage }}
+    </BaseDialog>
+    <BaseCard>
+      <section>
+        <div class="controls">
+          <BaseButton mode="outline" @click="loadCoaches(true)">Refresh</BaseButton>
+          <BaseButton to-name="register" v-if="canRegisterAsCoach">
+            Register as Coach
+          </BaseButton>
+        </div>
+
+        <BaseSpinner v-if="coachStore.isLoading" />
+        <ul v-else-if="hasCoaches">
+          <CoachItem
+            v-for="coach in filteredCoaches"
+            :coach="coach"
+            :key="coach.id"
+          />
+        </ul>
+        <h3 v-else>No coaches found.</h3>
+      </section>
+    </BaseCard>
+  </div>
 </template>
 
 <style scoped>
