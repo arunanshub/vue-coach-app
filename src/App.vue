791b3c64a8489db60104c78b2cc099c601c5dbee
Arunanshu Biswas <mydellpc07@gmail.com>
2023-05-02 12:58:06 +0530

    feat(App): add route transitions

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@ import TheHeader from '@/components/layout/TheHeader.vue'
 
 <template>
   <TheHeader />
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </Transition>
+  </RouterView>
 </template>
 
 <style>
@@ -20,5 +24,29 @@ html {
 
 body {
   margin: 0;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-leave-to {
+  opacity: 1;
+  transform: translateY(30px);
+}
+
+.route-enter-active {
+  transition: all 0.2s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.2s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
