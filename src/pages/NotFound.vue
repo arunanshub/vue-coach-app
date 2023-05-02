aba5516759995ed02d9f3ff5f8b513bd2fd0e4a9
Arunanshu Biswas <mydellpc07@gmail.com>
2023-05-02 12:57:43 +0530

    feat(NotFound): improve NotFound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,14 @@
-<template>
-  <h1>404</h1>
-  <h2>Page Not Found</h2>
+<script setup lang="ts">
+import BaseCard from '@/components/ui/BaseCard.vue'
+</script>
 
-  <NavLink to-name="coaches">Go Home</NavLink>
+<template>
+  <BaseCard>
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+    <p>
+      The page was not found. How about you check out
+      <NavLink to-name="coaches">the coaches?</NavLink>
+    </p>
+  </BaseCard>
 </template>
