8b3ec0dcb573c357e113da3346324bf10ac65807
Arunanshu Biswas <mydellpc07@gmail.com>
2023-05-13 00:29:24 +0530

    build: add support for purging css, autoprefixing and legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import vue from '@vitejs/plugin-vue'
-import { URL, fileURLToPath } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import purgecss from '@fullhuman/postcss-purgecss'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), legacy()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -12,5 +16,32 @@ export default defineConfig({
   },
   esbuild: {
     legalComments: 'none',
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(),
+        purgecss({
+          content: [`./*.html`, `./src/**/*.vue`],
+          defaultExtractor(content) {
+            const contentWithoutStyleBlocks = content.replace(
+              /<style[^]+?<\/style>/gi,
+              ''
+            )
+            return (
+              contentWithoutStyleBlocks.match(
+                /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+              ) || []
+            )
+          },
+          safelist: [
+            /-(leave|enter|appear)(|-(to|from|active))$/,
+            /^(?!(|.*?:)cursor-move).+-move$/,
+            /^router-link(|-exact)-active$/,
+            /data-v-.*/,
+          ],
+        }),
+      ],
+    },
   },
 })
